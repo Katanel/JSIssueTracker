555d212c86c48eed18aa34772f1ee1ac7245a1ee
Nelson Catalano <nelson.catalano88@gmail.com>
2018-05-27 22:16:02 +0200

    Js IssueTracker
    First commit

---
+++ main.js
@@ -0,0 +1,58 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var severityH = document.getElementById('severInput').value;
+    var assigned = document.getElementById('issueAssignInput').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: severityH,
+        assignedTo: assigned,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').requestFullscreen();
+
+    fetchIssues();
+
+    e.preventDefault();
+}
+
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assign = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well"></div>' +
+            '<h6>Issue ID: ' + id + '</h6>' +
+            '<p><span class="label label-info">' + status + '</span></p>' +
+            '<h3>' + desc + '</h3>' +
+            '<p><span class="glyphicon-time></span>' + severity + '</p>' +
+            '<p><span class="glyphicon-time></span>' + assign + '</p>' +
+            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warnign">Close</a>' +
+            '<a href="#" onclick="delete(\'' + id + '\')" class="btn btn-danger">Close</a>' +
+            '</div>';
+    }
+}
